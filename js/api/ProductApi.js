6609565965c826248ee413d833f52468c37e7db3
Bogdan <bogdanlitvin8@gmail.com>
2017-05-12 11:56:51 +0300

    [~] refactoring code in api

--- js/api/ProductApi.js
+++ js/api/ProductApi.js
@@ -8,8 +8,8 @@ class ProductApi {
         'Content-Type': 'application/json'
       }
     });
-    
-    return response = await response.text()
+
+    return response = await response.text();
   }
 
   /* Get all product with chose category */
@@ -21,33 +21,34 @@ class ProductApi {
         'Content-Type': 'application/json'
       }
     });
-    
-    return response = await response.text()
+
+    return response = await response.text();
   }
 
   /* Create product */
   static async createProduct(paramsProduct, jwt) {
-   let formData = new FormData();
-   formData.append('product[text]', paramsProduct.text);
-   formData.append('product[user_id]', paramsProduct.user_id);
-   formData.append('product[category_id]', paramsProduct.category_id);
-   formData.append('product[price]', paramsProduct.price);
-   if(paramsProduct.image.fileName){ // check if upload image
-     formData.append('product[image]', {
-       uri: paramsProduct.image.uri,
-       type: paramsProduct.image.type,
-       name: paramsProduct.image.fileName,
-     });
-   }
-   let response = await fetch('http://fshop.ustk.in.ua/products.json', {
-    method: 'POST',
-    headers: {
-      'AUTHORIZATION': `Bearer ${jwt}`,
-    },
-    body: formData
-  });
-   return response = JSON.parse(await response.text())
- }
+    let formData = new FormData();
+    formData.append('product[text]', paramsProduct.text);
+    formData.append('product[user_id]', paramsProduct.user_id);
+    formData.append('product[category_id]', paramsProduct.category_id);
+    formData.append('product[price]', paramsProduct.price);
+    if (paramsProduct.image.fileName) { // check if upload image
+      formData.append('product[image]', {
+        uri: paramsProduct.image.uri,
+        type: paramsProduct.image.type,
+        name: paramsProduct.image.fileName,
+      });
+    }
+    let response = await fetch('http://fshop.ustk.in.ua/products.json', {
+      method: 'POST',
+      headers: {
+        'AUTHORIZATION': `Bearer ${jwt}`,
+      },
+      body: formData
+    });
+
+    return response = JSON.parse(await response.text());
+  }
 }
 
-export default ProductApi
+export default ProductApi;
