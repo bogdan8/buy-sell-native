8bf40413df339a890923e8675af2c1b44db7af00
Bogdan <bogdanlitvin8@gmail.com>
2017-05-12 12:22:39 +0300

    [~] refactoring code in reducers

--- js/reducers/products.js
+++ js/reducers/products.js
@@ -1,10 +1,8 @@
-import {
-  GET_ALL_PRODUCTS
-} from '../actions/types';
+import * as types from '../actions/types';
 
 export default function products(state = [], action) {
   switch (action.type) {
-    case GET_ALL_PRODUCTS:
+    case types.GET_ALL_PRODUCTS:
       return action.products;
     default:
       return state;
--- js/reducers/users.js
+++ js/reducers/users.js
@@ -1,8 +1,8 @@
-import {GET_ALL_USERS} from '../actions/types';
+import * as types from '../actions/types';
 
 export default function users(state = [], action) {
   switch (action.type) {
-    case GET_ALL_USERS:
+    case types.GET_ALL_USERS:
       return action.users;
     default:
       return state;
